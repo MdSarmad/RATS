d82f071ae05928f8a3109657f86308a9b125f68c
Nandini Ojha <nandiniojha1822@gmail.com>
2022-05-09 22:59:11 +0530

    ui changes

--- src/Components/Section/SectionA1.js
+++ src/Components/Section/SectionA1.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './SectionA1.css';
-
+import Img from '../../images/section1.jpg';
 function SectionA1() {
   return (
     <div className='section-a1'>
-        <div className="section-a1-text">
-            <h1 className='section-a1-title'>RATS</h1>
-            <h3 className='section-a1-subTitle'>A Technology Stack Recommender</h3>
-            <p className='section-a1-para'>One platform where for all those who need guidance while choosing the right technolog stack for building their application</p>
-        </div>
-        <div className="section-a1-pic"></div>
+      <div className="section-a1-text">
+        <h1 className='section-a1-title'>RATS</h1>
+        <h3 className='section-a1-subTitle'>A Technology Stack Recommender</h3>
+        <p className='section-a1-para'>One platform where for all those who need guidance while choosing the right technolog stack for building their application</p>
+      </div>
+      <div className="section-a1-pic">
+        <img className="section1-img" src={Img} alt="image" />
+      </div>
     </div>
   )
 }
--- src/Components/Section/SectionA2.js
+++ src/Components/Section/SectionA2.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import './SectionA2.css';
+import frontend from '../../images/frontend-img.jpg';
+import backend from '../../images/backend-img.png';
+import fullstack from '../../images/fullstack-img.jpg';
+
 
 function SectionA2() {
     return (
         <div className='section-a2'>
-            <h1 className='section-a2-heading'>What kind of website are you building </h1>
+            <h1 className='section-a2-heading'>What kind of website are you building?</h1>
             <div className="tech-section">
-                <div className="frontend tech">
-                    <div className="frontend-logo tech-logo"></div>
+                <div className="frontend-tech">
+                    <div className="frontend-logo tech-logo">
+                        <img className="frontend-img" src={frontend} />
+                    </div>
                     <div className="frontend-text tech-text">Frontend</div>
                 </div>
-                <div className="backend tech">
-                    <div className="backend-logo tech-logo"></div>
+                <div className="backend-tech">
+                    <div className="backend-logo tech-logo">
+                        <img className="backend-img" src={backend} />
+                    </div>
                     <div className="backend-text tech-text">Backend</div>
                 </div>
-                <div className="full-stack tech">
-                    <div className="full-stack-logo tech-logo"></div>
+                <div className="full-stack-tech">
+                    <div className="full-stack-logo tech-logo">
+                        <img className="fullstack-img" src={fullstack} />
+                    </div>
                     <div className="full-stack-text tech-text">Full Stack</div>
                 </div>
             </div>
