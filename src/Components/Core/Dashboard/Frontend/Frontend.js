d00a65415e2ec400b0cc0d08df31e94f52e0dc9c
Md Sarmad <28mdsarmad99@gmail.com>
2022-05-16 05:55:30 +0530

    frontend-updated

--- src/Components/Core/Dashboard/Frontend/Frontend.js
+++ src/Components/Core/Dashboard/Frontend/Frontend.js
@@ -1,103 +1,114 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Frontend.css";
 import frontendImg from '../../../../images/stack/fullstack6.png'
 
 function Frontend() {
+  const [webSize, setWebSize] = useState("");
+  const [webRender, setWebRender] = useState("");
+  const [webType, setWebType] = useState("");
+  const [webArchitecture, setWebArchitecture] = useState("");
+
+  const getFrontendTechStack = () => {
+    console.log(webSize,webRender,webType,webArchitecture);
+  }
+
   return (
     <div>
       <div className='frontendDashboard'>
         <div className='frontendDashboardBody'>
           <div class="frontendDashboardTitle">MY FRONTEND TECH STACK</div>
-
           <div className='frontendDashboardContainer'>
             <div className='frontendDashboardLeftContainer'>
               <div className='frontendDashboardContent'>
-                <div className='markupBody'>
 
-                  <div className='markupLanguageSelect'>
+                <div className='markupBody web-size-box'>
+                  <div className='markupLanguageSelect web-size-body'>
                     <select
                       typeof='option'
-                      id='markupLanguage'
+                      id='markupLanguage web-size-id'
                       select
-                      className='markupLanguage'
+                      className='markupLanguage web-size-class'
+                      value={webSize}
+                      onChange={(e) => setWebSize(e.target.value)}
                       autoFocus
                       autoComplete
                       required
                     >
-                      <option value={''}>Markup Languages</option>
-                      <option value={'HTML'}>Are you kidding?</option>
-                      <option value={'HTML'}>Do you want to go back in time?</option>
-                      <option value={'HTML'}>HTML</option>
-                      <option value={'XHTML'}>Still you want something stringent, polished, and straightforward</option>
+                      <option value={''}>What is the size of the website?</option>
+                      <option value={'SS'}>Small and Single Page</option>
+                      <option value={'SM'}>Small with multiple Pages</option>
+                      <option value={'LS'}>Medium/Large with Single Page</option>
+                      <option value={'LM'}>Medium/Large with Multiple Pages</option>
                     </select>
                   </div>
 
                 </div>
 
 
-                <div className='stylingBody'>
+                <div className='stylingBody web-render-box'>
 
-                  <div className='stylingSelect'>
+                  <div className='stylingSelect web-render-body'>
                     <select
                       typeof='option'
-                      id='stylingLanguage'
+                      id='stylingLanguage web-render-id'
                       select
-                      className='stylingLanguage'
+                      className='stylingLanguage web-render-class'
+                      value={webRender}
+                      onChange={(e)=>setWebRender(e.target.value)}
                       autoFocus
                       autoComplete
                       required
                     >
-                      <option value={''}>Styling</option>
-                      <option value={'CSS'}>CSS</option>
-                      <option value={'Tailwind'}>Tailwind CSS</option>
-                      <option value={'Bootstrap'}>Bootstrap</option>
+                      <option value={''}>Rendering</option>
+                      <option value={'SSR'}>Server Side Rendering</option>
+                      <option value={'CSR'}>Client Side Rendering</option>
                     </select>
                   </div>
 
                 </div>
 
 
-                <div className='programmingBody'>
+                <div className='programmingBody web-type-box'>
 
-                  <div className='programmingSelect'>
+                  <div className='programmingSelect web-type-body'>
                     <select
                       typeof='option'
-                      id='programmingLanguage'
+                      id='programmingLanguage web-type-id'
                       select
-                      className='programmingLanguage'
+                      className='programmingLanguage web-type-class'
+                      value={webType}
+                      onChange={(e)=>setWebType(e.target.value)}
                       autoFocus
                       autoComplete
                       required
                     >
-                      <option value={''}>Programming Languages</option>
-                      <option value={'javascript'}>Java Script</option>
-                      <option value={'Python'}>Python</option>
-                      <option value={'Java'}>Java</option>
-                      <option value={'PHP'}>PHP</option>
+                      <option value={''}>Static or Dynamic?</option>
+                      <option value={'static'}>Static Website</option>
+                      <option value={'dynamic'}>Dynamic Website</option>
                     </select>
                   </div>
 
                 </div>
 
 
-                <div className='libraryBody'>
+                <div className='libraryBody web-arch-box'>
 
-                  <div className='librarySelect'>
+                  <div className='librarySelect web-arch-body'>
                     <select
                       typeof='option'
-                      id='library'
+                      id='library web-arch-id'
                       select
-                      className='library'
+                      className='library web-arch-class'
+                      value={webArchitecture}
+                      onChange={(e)=>setWebArchitecture(e.target.value)}
                       autoFocus
                       autoComplete
                       required
                     >
-                      <option value={''}>Library</option>
-                      <option value={'Angular'}>Angular</option>
-                      <option value={'React'}>React</option>
-                      <option value={'Vue'}>Vue</option>
-                      <option value={'React_Native'}>React Native</option>
-                      <option value={'Lavarel'}>Lavarel</option>
+                      <option value={''}>Architecture</option>
+                      <option value={'MVC'}>Model-View-Controller</option>
+                      <option value={'MVVM'}>Model-View-ViewModel</option>
+                      <option value={'FLUX'}>Flux</option>
                     </select>
                   </div>
 
@@ -107,7 +118,7 @@ function Frontend() {
               </div>
 
               <div className='gateTechStackBody'>
-                <button className='gateTechStackButton'>Get Teck Stack</button>
+                <div className='gateTechStackButton' onClick={getFrontendTechStack}>Get Teck Stack</div>
               </div>
 
               <div className='frontendDashboardAnswer'>
@@ -116,7 +127,7 @@ function Frontend() {
 
             </div>
             <div className='frontendDashboardRightContainer'>
-              <img className="dashboard-frontend-img" src={frontendImg} alt="image" />
+              <img className="dashboard-frontend-img" src={frontendImg} alt="dashboard-frontend-img" />
             </div>
           </div>
 
