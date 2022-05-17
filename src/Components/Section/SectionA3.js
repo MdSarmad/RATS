2484a1b97a6d7504e9c059a705c7f812147fa112
Md Sarmad <28mdsarmad99@gmail.com>
2022-05-17 09:36:50 +0530

    code updated

--- src/Components/Section/SectionA3.js
+++ src/Components/Section/SectionA3.js
@@ -12,7 +12,7 @@ function SectionA3() {
             <div className="section-a3-reviews">
                 <div className="review1 review">
                     <div className="review1-logo review-logo">
-                        <img className="ross-img" src={Ross} />
+                        <img className="ross-img" src={Ross} alt={"ross-img"} />
                     </div>
                     <div className="review1-text review-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error numquam sint at voluptatem voluptate suscipit est accusantium repellendus, quisquam nihil?</div>
                     <div className="review1-reviewer review-reviewer">
@@ -21,7 +21,7 @@ function SectionA3() {
                 </div>
                 <div className="review2 review">
                     <div className="review2-logo review-logo">
-                        <img className="joey-img" src={Joey} />
+                        <img className="joey-img" src={Joey} alt={"joey-img"}/>
                     </div>
                     <div className="review2-text review-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ea et deserunt accusantium enim adipisci architecto dolorum officiis omnis labore!</div>
                     <div className="review2-reviewer review-reviewer">
@@ -30,7 +30,7 @@ function SectionA3() {
                 </div>
                 <div className="review3 review">
                     <div className="review3-logo review-logo">
-                        <img className="chandler-img" src={Chandler} />
+                        <img className="chandler-img" src={Chandler} alt={"chandler-img"} />
                     </div>
                     <div className="review3-text review-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor tempore nihil officia quisquam quod consequatur maiores veritatis doloribus vitae et!</div>
                     <div className="review3-reviewer review-reviewer">
